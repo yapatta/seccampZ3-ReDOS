9a538ecdf042777dcdb40ca98495599bf3d52280
yuziroppe <syani4732@gmail.com>
2020-10-31 17:40:50 +0900

    Sequence, Disjunction, Charまで完了

--- src/nfa.ts
+++ src/nfa.ts
@@ -1,6 +1,6 @@
 import { Node, Parser } from 'rerejs';
 
-const testText = '(a|b)*cd';
+const testText = 'a|b';
 
 export type NFA = AutomatonNode;
 
@@ -17,25 +17,49 @@ export const createNFA = (exp: string): void => {
   const parser = new Parser(exp);
   const pattern = parser.parse();
 
-  createAutomatonNode(pattern);
+  const nfa = createAutomatonNode(pattern.child);
   // pattern.child.children.map((x) => console.log(x));
-  console.log(typeof pattern.child);
+  console.log(nfa.next[0].node);
 };
 
 const createAutomatonNode = (node: Node): AutomatonNode => {
   switch (node.type) {
     case 'Disjunction':
+      {
+        const nextNodes = node.children.map((child) =>
+          createAutomatonNode(child),
+        );
+
+        const nowNode: AutomatonNode = { next: [] };
+
+        nextNodes.map((nn) => {
+          const arrow: Arrow = { char: '', node: nn };
+          nowNode.next.push(arrow);
+        });
+
+        return nowNode;
+      }
       break;
     case 'Sequence': {
+      const seqNodes = node.children.map((child) => createAutomatonNode(child));
+
+      for (let i = 0; i < seqNodes.length - 1; i++) {
+        const before = seqNodes[i];
+        const after = seqNodes[i + 1];
+        before.next.push({ char: '', node: after } as Arrow);
+      }
+
+      return seqNodes[0];
     }
     case 'Char': {
-      const nextNodes: AutomatonNode = { next: [] };
-      const arrow: Arrow = { char: node.raw, node: nextNodes };
-      const nowNodes: AutomatonNode = { next: [arrow] };
+      const nextNode: AutomatonNode = { next: [] };
+      const arrow: Arrow = { char: node.raw, node: nextNode };
+      const nowNode: AutomatonNode = { next: [arrow] };
 
-      return nowNodes;
+      return nowNode;
     }
   }
+  return {} as AutomatonNode;
 };
 
 // Test
